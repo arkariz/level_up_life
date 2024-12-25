008522c431f1a8d65a8648030d2561d431661f9d
arkariz <muhammadrisky1401@gmail.com>
2024-12-25 20:30:46 +0700

    [PRESENTATION] feat: add custom toast success

--- lib/presentation/core/widgets/toast/custom_toast.dart
+++ lib/presentation/core/widgets/toast/custom_toast.dart
@@ -10,4 +10,13 @@ class CustomToast {
       style: ToastificationStyle.fillColored
     );
   }
+
+  static Duration? showSuccess(String message) {
+    return toastification.show(
+      description: Text(message),
+      autoCloseDuration: const Duration(seconds: 5),
+      type: ToastificationType.success,
+      style: ToastificationStyle.fillColored
+    ).autoCloseDuration;
+  }
 }
