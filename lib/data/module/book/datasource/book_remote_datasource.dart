d7e4f86cfa6b7bd5a5bab0be4d8480c76d3f99d4
arkariz <muhammadrisky1401@gmail.com>
2024-12-26 18:11:19 +0700

    chores: delete example

--- lib/data/module/book/datasource/book_remote_datasource.dart
+++
@@ -1,6 +0,0 @@
-import 'package:level_up_life/data/module/book/model/book_model.dart';
-import 'package:level_up_life/domain/module/book/request/request_book.dart';
-
-abstract class BookRemoteDatasource {
-  Future<BooksModel> getBooks(RequestBook request);
-}
