10260ee1c0de5c847aa1b5cd638c921155175da2
arkariz <muhammadrisky1401@gmail.com>
2024-12-29 16:10:20 +0700

    [DATA] feat: base service add parameter list data for post many

--- lib/data/base_service/data_class/requests/post_request.dart
+++ lib/data/base_service/data_class/requests/post_request.dart
@@ -4,13 +4,14 @@ part of 'base_request.dart';
 Future<dynamic> PostRequest(
   Dio? dio, {
   bool isReturnJson = true,
+  List<JSON>? listData,
   required String url,
   required JSON headers,
   required JSON data,
 }) async {
   final response = await dio!.post(
     url,
-    data: jsonEncode(data),
+    data: jsonEncode(listData ?? data),
     options: Options(
         headers: headers,
         responseType: isReturnJson ? ResponseType.json : ResponseType.bytes),
